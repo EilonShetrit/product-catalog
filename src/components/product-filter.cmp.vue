38ba73fb3666bbaf95ad0d4f732f2a26c502a3ee
Eilon Shetrit <eilonshetrit@gmail.com>
2020-12-10 18:09:02 +0200

    יעחיע

--- src/components/product-filter.cmp.vue
+++ src/components/product-filter.cmp.vue
@@ -1,12 +1,48 @@
 <template>
   <div class="product-filter">
-    
+  <el-select v-model="price" placeholder="Select">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :value="item.value">
+    </el-option>
+  </el-select>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'productApp',
+  name: 'productFilter',
+  data() {
+    return {
+      price: 'All',
+      options:[{value:'up to 25'},{value:'25 to 50'},{value:'50 to 75'},{value:'75 to 100'},{value:'100+'},{value:'All'}]
+    }
+  },
+  computed: {
+    filterdProducts() {
+      if (this.price === 'All') return this.stores
+      var res = stores.filter((store) => {
+        store.filter((product) => {
+          switch(this.price){
+            case 'up to 25':
+              return this.product.price < 25; 
+            case '25 to 50':
+              return this.product.price > 25 && this.product.price < 50; 
+            case '50 to 75':
+              return this.product.price > 50 && this.product.price < 75; 
+            case '75 to 100':
+              return this.product.price > 75 && this.product.price < 100; 
+            case '100+':
+              return this.product.price > 100; 
+            case 'All':
+              return this.stores  
+          }
+        });
+
+      });
+    }
+  }
 }
 </script>
--- src/components/product-list.cmp.vue
+++ src/components/product-list.cmp.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="product-list">
+    <product-filter />
     <ul v-if="stores">
       <li v-for="store in stores" :key="store.StoreId">
         <div v-for="store in stores" :key="store">
@@ -13,35 +14,23 @@
 
 <script>
 import storePreview from '@/components/store-preview.cmp.vue';
+import productFilter from '@/components/product-filter.cmp.vue';
 export default {
   name: 'productApp',
   data(){
     return{
-      gendre:'all',
     }
   },
   computed:{
     stores(){
       return this.$store.getters.getStores;
     },
-    filterdStores(){
-      if(this.gendre === 'all') return this.stores
-      	var res = stores.filter((store) => {
-          store.filter((product) => {
-            // return product.gendre.toLowerCase() === this.gendre.toLowerCase();
-             if (product.price > this.min && product.price < this.max){
-                return product
-            }
-          });
-				//console.log('item',item.genre)
-			
-			});
-    }
   },
   created(){
   },
   components:{
-    storePreview
+    storePreview,
+    productFilter
   }
 }
 </script>
